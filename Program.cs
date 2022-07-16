5bbdd0cfa0657c67b3181906cdecab81a39ca996
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-16 08:42:05 +0300

    Basic package setup

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace ExcelDemo2;
+﻿using System;
+using System.ComponentModel;
+using OfficeOpenXml;
+using System.IO;
+using static System.Environment;
+
+namespace ExcelDemo2;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
+        ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+        var file = new FileInfo(SpecialFolder.Desktop.ToString());
     }
 }
