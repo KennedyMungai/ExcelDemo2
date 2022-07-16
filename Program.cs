4e705cb2302a31ce3b7430e3650e5337b3fb0333
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-16 08:52:23 +0300

    Created the Person Model class

--- Program.cs
+++ Program.cs
@@ -15,3 +15,10 @@ public class Program
         var file = new FileInfo(Path.Combine(SpecialFolder.Desktop + "ExcelSucks.xlsx"));
     }
 }
+
+public class PersonModel
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
